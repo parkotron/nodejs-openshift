2a8fa669cb19f008d8b65d3ee937e7c9c812a4b9
Ian Parkins <parkotron@gmail.com>
2013-07-23 13:28:01 +0100

    underscore ref update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var
   express = require('express'),
-  _ = require('underscore'),
+  _ = require('underscore')._,
   port = process.argv[2] || 8080,
   ip = process.argv[3] || '127.0.0.1',
   app = express();
