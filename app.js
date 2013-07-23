c1999d0c2c88690634e6923feef62583621641b0
Ian Parkins <parkotron@gmail.com>
2013-07-23 14:12:20 +0100

    remove underscore

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 var
   express = require('express'),
-  _ = require('underscore')._,
   port = process.argv[2] || 8080,
   ip = process.argv[3] || '127.0.0.1',
   app = express();
@@ -15,7 +14,7 @@ app.configure(function () {
 app.get('/', function (req, res, next) {
   res.render('index', { 
   	title: 'A test app', 
-  	score: _.once(function(){ return "hello world"; }),
+  	score: 'hello world',
   	node_version: process.version 
   });
 });
